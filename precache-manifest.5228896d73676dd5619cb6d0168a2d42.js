3e88c414d8e24689f62361acc4f6c0301578e2fe
Sonmaster <congson1112@gmal.com>
2020-02-17 15:42:21 +0700

    Updates

---
+++ precache-manifest.5228896d73676dd5619cb6d0168a2d42.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f34bbee07b3d61f0e3f4ce15471c5aaa",
+    "url": "/sonmasterj/robot/index.html"
+  },
+  {
+    "revision": "8d7ceafdf43fb2b45734",
+    "url": "/sonmasterj/robot/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "236aa6290e6bcec8f866",
+    "url": "/sonmasterj/robot/static/css/main.b58e04c4.chunk.css"
+  },
+  {
+    "revision": "8d7ceafdf43fb2b45734",
+    "url": "/sonmasterj/robot/static/js/2.01e51e32.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/sonmasterj/robot/static/js/2.01e51e32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "236aa6290e6bcec8f866",
+    "url": "/sonmasterj/robot/static/js/main.dc0ade9e.chunk.js"
+  },
+  {
+    "revision": "116a364db278be950b22",
+    "url": "/sonmasterj/robot/static/js/runtime-main.c4cda7a5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/sonmasterj/robot/static/media/SEGA.12a3db8b.woff"
+  }
+]);
